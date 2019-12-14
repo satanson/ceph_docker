731e959073208ea60cdfebd7e681b8616273adca
satanson <ranpanf@gmail.com>
2019-12-14 22:10:47 +0800

    add mk_disk.sh

---
+++ scripts/rm_rbd.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e -o pipefail
+basedir=$(cd $(dirname $(readlink -f ${BASH_SOURCE:-$0}));pwd)
+cd ${basedir}
+
+pool=${1:?"missing 'pool'"};shift
+image=${1:?"missing 'image'"};shift
+
+
+exists=$(rbd device list |perl -lne "print 1 if /\\b${pool}\\b.*\\b(${image})\\b/")
+if [ -n "${exists}" ];then
+  rbd device unmap ${pool}/${image}
+fi
+exists=$(rbd device list |perl -lne "print 1 if /\\b${pool}\\b.*\\b(${image})\\b/")
+test -z "${exists}"
+
+if rbd info ${pool}/${image};then
+  rbd remove ${pool}/${image}
+fi
+
+rbd ls 
